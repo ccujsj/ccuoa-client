80c7251b52eef9f4077fab183a96016859ce27f2
phil616 <phil616@163.com>
2023-09-09 20:39:49 +0800

    add new examples

--- example/export_moral.py
+++ example/export_moral.py
@@ -1,9 +1,20 @@
 from mapping_model import load_excel_to_model
+from schemas import StudentSchema
 from session import ClintSession
 
 session = ClintSession()
 session.update_baseurl("http://localhost:8000")
 session.set_bearer("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3IiOiJhZG1pbiIsInNjb3BlIjpbInN0dWRlbnQiLCJzdGFmZiIsInN5c3RlbSJdLCJleHAiOjE2OTQzMzc3MzZ9.KWGThipWNm9e8sgC8BF-dSGP-C49EZ3F9Te7cnIR51U")
-record_type = 4
-r = session.get(f"/api/v1/get/moral/record/byType?t={record_type}")
-print(r.json())
+
+def get_all_record():
+    record_type = 4
+    r = session.get(f"/api/v1/get/moral/record/byType?t={record_type}")
+    d = r.json()
+    for i in d:
+        print(i)
+def get_student_score(stu_id):
+    l = session.get(f"/api/v1/get/student/info/id?stu_id={stu_id}")
+    s = StudentSchema(**l.json())
+    print(s.model_dump())
+
+get_student_score("042040315")
---
+++ example/info.py
@@ -0,0 +1,14 @@
+from schemas import TextSchema
+from session import ClintSession
+session = ClintSession()
+session.update_baseurl("http://localhost:8000")
+session.set_bearer("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3IiOiJhZG1pbiIsInNjb3BlIjpbInN0dWRlbnQiLCJzdGFmZiIsInN5c3RlbSJdLCJleHAiOjE2OTQzMzc3MzZ9.KWGThipWNm9e8sgC8BF-dSGP-C49EZ3F9Te7cnIR51U")
+
+def upload_text(text_key:str,text_value:str,text_type:int=0):
+    info = TextSchema(text_key=text_key,
+                      text_value=text_value,
+                      text_type=text_type)
+    r = session.post("/api/v1/put/info/text",json=info.model_dump(mode='json'))
+    print(r)
+
+upload_text('name','hello')
